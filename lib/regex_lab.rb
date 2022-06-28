419e040dc685a9e9eca660c7c893e8b26b92df8f
Harrison Sabean <hsabes@gmail.com>
2022-06-28 13:55:12 -0700

    commit tree a3084e603d9417593274b1a24d931418a5043fa9

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,10 @@
 def starts_with_a_vowel?(word)
-
+    if (word[0].match(/[aeiou]/))
+        true
+    end
 end
+
+# starts_with_a_vowel?('face')
 
 def words_starting_with_un_and_ending_with_ing(text)
 
